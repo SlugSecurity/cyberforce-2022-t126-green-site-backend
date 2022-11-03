01d2d93e4fb7b83a20dbc3d4d868dba011ab827f
Steven Mak <stmak@ucsc.edu>
2022-11-03 14:55:17 -0700

    Add more error constants

--- green-site-backend/src/error.rs
+++ green-site-backend/src/error.rs
@@ -2,6 +2,12 @@ use serde::Serialize;
 use std::{error::Error, fmt::Display, io};
 use ServerConfigError::*;
 
+pub(crate) const INTERNAL_ERROR: &str =
+    "Internal server error encountered. Please try again later.";
+
+pub(crate) const NO_ENV_VAR_APP_DATA: &str =
+    "Couldn't find any environment variable app data. This should never happen.";
+
 #[derive(Debug)]
 pub(crate) enum ServerConfigError {
     ReadPemIoError(String, io::Error),
