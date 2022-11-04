7eed47c47cd1e68ba527b09f00a37d3db081af08
Steven Mak <stmak@ucsc.edu>
2022-11-04 03:49:22 -0700

    Improve has_admin_token

--- green-site-backend/src/token.rs
+++ green-site-backend/src/token.rs
@@ -1,3 +1,4 @@
+use actix_web::{http::header, HttpRequest};
 use serde::Serialize;
 
 use crate::env_vars::BackendVars;
@@ -13,6 +14,14 @@ impl AdminToken {
 }
 
 /// Verifies the token is that of the admin account.
-pub(crate) fn has_admin_token(token: AdminToken, env_var: BackendVars) -> bool {
-    env_var.admin_token == token.0
+pub(crate) fn has_admin_token(req: HttpRequest, env_var: BackendVars) -> bool {
+    let auth_header = req.headers().get(header::AUTHORIZATION);
+
+    auth_header
+        .and_then(|auth| auth.to_str().ok())
+        .filter(|auth_str| {
+            auth_str.starts_with("Bearer ")
+                && auth_str.get(7..) == Some(env_var.admin_token.as_str())
+        })
+        .is_some()
 }
