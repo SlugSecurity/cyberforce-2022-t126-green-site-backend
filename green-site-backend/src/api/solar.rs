8e20c0b93ad3604c52bfd59d436f3cfc51d1a986
Steven Mak <stmak@ucsc.edu>
2022-11-04 04:59:20 -0700

    Fix solar endpoint

--- green-site-backend/src/api/solar.rs
+++ green-site-backend/src/api/solar.rs
@@ -7,14 +7,20 @@ use crate::env_vars::BackendVars;
 use crate::error::{self, MISSING_APP_DATA};
 
 #[derive(FromRow, Serialize)]
-#[sqlx(rename_all = "camelCase")]
 struct SolarPanelInfo {
+    #[sqlx(rename = "arrayID")]
     array_id: i32,
+    #[sqlx(rename = "solarStatus")]
     solar_status: String,
+    #[sqlx(rename = "arrayVoltage")]
     array_voltage: i32,
+    #[sqlx(rename = "arrayCurrent")]
     array_current: i32,
+    #[sqlx(rename = "arrayTemp")]
     array_temp: i32,
+    #[sqlx(rename = "trackerTilt")]
     tracker_tilt: i32,
+    #[sqlx(rename = "trackerAzimuth")]
     tracker_azimuth: i32,
 }
 
@@ -23,12 +29,15 @@ async fn get_solar_panel_info(
     vars: &BackendVars,
 ) -> sqlx::Result<Vec<SolarPanelInfo>> {
     sqlx::query_as(
-        "SELECT arrayID, solarStatus, \
+        format!(
+            "SELECT arrayID, solarStatus, \
              arrayVoltage, arrayCurrent, arrayTemp, \
              trackerTilt, trackerAzimuth \
-         FROM ?",
+         FROM {};",
+            vars.data_historian_db_table.as_str(),
+        )
+        .as_str(),
     )
-    .bind(vars.data_historian_db_table.as_str())
     .persistent(true)
     .fetch_all(pool)
     .await
