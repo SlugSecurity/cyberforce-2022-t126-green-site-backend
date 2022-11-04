45c664c81dc22c47aefa1dedd0149579e6688246
Steven Mak <stmak@ucsc.edu>
2022-11-04 14:28:42 -0700

    Finish get file(s) endpoints

--- green-site-backend/src/api/files.rs
+++ green-site-backend/src/api/files.rs
@@ -3,11 +3,20 @@ use actix_web::{
     web::{Path, ServiceConfig},
     HttpMessage, HttpRequest, HttpResponse, Responder,
 };
+
+use async_std::io::ReadExt;
+use bytes::Bytes;
+use futures::{
+    io::AsyncBufReadExt,
+    stream::{self, once},
+    AsyncRead, Stream, StreamExt,
+};
 use log::error;
 use serde::Serialize;
 use suppaftp::{
     async_native_tls::{Certificate, Protocol, TlsConnector},
-    FtpResult, FtpStream, TlsConnector as FtpTlsConnector,
+    types::Response,
+    FtpError, FtpResult, FtpStream, Status, TlsConnector as FtpTlsConnector,
 };
 
 use crate::{
@@ -50,26 +59,50 @@ macro_rules! verify_var_cert {
 }
 
 #[derive(Serialize)]
-struct File {}
+struct File {
+    name: String,
+    id: String,
+    size: u64,
+}
+
+fn split_name(name: &str) -> Option<(String, String)> {
+    let split = name
+        .find('-')
+        .filter(|&idx| name[..idx].bytes().all(|c| c.is_ascii_digit()) && name.len() > idx + 1)?;
+
+    Some((name[..split].to_string(), name[split + 1..].to_string()))
+}
+
+async fn list_files(var: &BackendVars, cert: &Certificate) -> FtpResult<Vec<File>> {
+    use suppaftp::list::File as FtpFile;
+
+    let file_list = secure_ftp_login(var, cert).await?.list(None).await?;
+    let mut processed_files = Vec::new();
+
+    for file in file_list {
+        let ftp_file = FtpFile::try_from(file).map_err(|_| FtpError::BadResponse)?;
+        let (name, id) = match split_name(ftp_file.name()) {
+            Some(pair) => pair,
+            None => return Err(FtpError::BadResponse),
+        };
+        let processed_file = File {
+            name,
+            id,
+            size: ftp_file.size() as u64,
+        };
+
+        processed_files.push(processed_file)
+    }
+
+    Ok(processed_files)
+}
 
 #[get("")]
 async fn get_files(req: HttpRequest) -> impl Responder {
-    async fn list_files(var: &BackendVars, cert: &Certificate) -> FtpResult<Vec<File>> {
-        use suppaftp::list::File as FtpFile;
-
-        let file_list = secure_ftp_login(var, cert).await?.list(None).await?;
-
-        for file in file_list {
-            let ftp_file = FtpFile::try_from(file);
-        }
-
-        todo!()
-    }
-
     let (var, cert) = verify_var_cert!(req);
 
     match list_files(var, cert).await {
-        Ok(file) => HttpResponse::Ok().json(file),
+        Ok(files) => HttpResponse::Ok().json(files),
         Err(err) => {
             error!("Encountered internal error while listing files from FTP server: {err}");
 
@@ -96,11 +129,64 @@ async fn upload_file(req: HttpRequest) -> impl Responder {
 }
 
 #[get("/{file_id}")]
-async fn get_file_by_id(req: HttpRequest, path: Path<u64>) -> impl Responder {
-    let (var, cert) = verify_var_cert!(req);
-    let file_id = path.into_inner();
+async fn get_file_by_id(req: HttpRequest, path: Path<u128>) -> impl Responder {
+    async fn download_file(
+        vars: &BackendVars,
+        tls_cert: &Certificate,
+        name: String,
+    ) -> FtpResult<(impl AsyncRead, FtpStream)> {
+        let mut conn = secure_ftp_login(vars, tls_cert).await?;
 
-    HttpResponse::Ok().body("")
+        Ok((conn.retr_as_stream(name).await?, conn))
+    }
+
+    let (var, cert) = verify_var_cert!(req);
+    let file_id = path.to_string();
+
+    let files = match list_files(var, cert).await {
+        Ok(files) => files,
+        Err(err) => {
+            error!("Encountered internal error while listing files from FTP server: {err}");
+
+            return internal_server_error();
+        }
+    };
+    let found_file = match files.into_iter().find(|f| f.id == file_id) {
+        Some(file) => file,
+        None => {
+            return HttpResponse::NotFound().json(ErrorResponse {
+                error: "Couldn't find requested file by ID".to_string(),
+            })
+        }
+    };
+
+    match download_file(var, cert, format!("{}-{}", found_file.id, found_file.name)).await {
+        Ok((mut data, mut stream)) => {
+            let mut data_vec = Vec::new();
+            data.read_to_end(&mut data_vec);
+
+            if let Err(err) = stream.finalize_retr_stream(data).await {
+                error!("We couldn't finalize the FTP stream: {err}");
+
+                internal_server_error()
+            } else {
+                HttpResponse::Ok().body(data_vec)
+            }
+        }
+        Err(err) => match err {
+            FtpError::UnexpectedResponse(Response {
+                status: Status::BadFilename,
+                ..
+            }) => HttpResponse::BadRequest().json(ErrorResponse {
+                error: format!("File with requested ID doesn't exist."),
+            }),
+            err => {
+                error!("We had an internal error while trying to get our FTP file: {err}");
+
+                internal_server_error()
+            }
+        },
+    }
 }
 
 pub(crate) fn file_endpoint_config(cfg: &mut ServiceConfig) {
