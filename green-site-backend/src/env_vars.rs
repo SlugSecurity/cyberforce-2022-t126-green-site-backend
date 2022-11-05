97921a38ff696bce4d45b79570154c31eb91e857
Steven Mak <stmak@ucsc.edu>
2022-11-04 17:14:05 -0700

    Remove extraneous rustls comments

--- green-site-backend/src/env_vars.rs
+++ green-site-backend/src/env_vars.rs
@@ -23,7 +23,5 @@ use green_site_backend_macros::env_vars;
 #[env_var("DEFAULT_RATE_LIMIT", usize)]
 #[env_var("ADMIN_ACCOUNT_USERNAME", String)]
 #[env_var("ADMIN_TOKEN", String)]
-// #[env_var("SSL_CERTIFICATE_PEM_PATH", String)]
-// #[env_var("SSL_PRIVATE_KEY_PEM_PATH", String)]
 #[env_var("ROOT_CERTIFICATE_PATH", String)]
 pub(crate) struct BackendVars;
--- green-site-backend/src/main.rs
+++ green-site-backend/src/main.rs
@@ -17,40 +17,6 @@ mod env_vars;
 mod error;
 mod token;
 
-// fn get_web_server_cert(vars: &BackendVars) -> Result<ServerConfig, CertConfigError> {
-//     use CertConfigError::*;
-
-//     let config = ServerConfig::builder()
-//         .with_safe_defaults()
-//         .with_no_client_auth();
-
-//     let cert_path = vars.ssl_certificate_pem_path.as_str();
-//     let cert_file = match File::open(cert_path) {
-//         Ok(ok) => ok,
-//         Err(err) => return Err(ReadPemIoError(cert_path.to_string(), err)),
-//     };
-//     let key_path = vars.ssl_private_key_pem_path.as_str();
-//     let key_file = match File::open(key_path) {
-//         Ok(ok) => ok,
-//         Err(err) => return Err(ReadPemIoError(key_path.to_string(), err)),
-//     };
-//     let cert_chain = rustls_pemfile::certs(&mut BufReader::new(cert_file))
-//         .map_err(|e| ReadPemIoError(cert_path.to_string(), e))?
-//         .into_iter()
-//         .map(RustCertificate)
-//         .collect::<Vec<_>>();
-
-//     let key_item = rustls_pemfile::read_one(&mut BufReader::new(key_file))
-//         .map_err(|e| ReadPemIoError(key_path.to_string(), e))?
-//         .ok_or_else(|| UnrecognizedPrivateKey(key_path.to_string()))?;
-//     let key = match key_item {
-//         X509Certificate(k) | RSAKey(k) | PKCS8Key(k) | ECKey(k) => PrivateKey(k),
-//         _ => return Err(UnrecognizedPrivateKey(key_path.to_string())),
-//     };
-
-//     Ok(config.with_single_cert(cert_chain, key)?)
-// }
-
 fn get_trusted_roots(vars: &BackendVars) -> Result<FtpCertificate, CertConfigError> {
     use CertConfigError::*;
 
@@ -86,7 +52,6 @@ fn create_pool(vars: &BackendVars) -> MySqlPool {
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let backend_vars = BackendVars::new()?;
-    // let rustls_server_config = get_web_server_cert(&backend_vars)?;
     let port = backend_vars.web_server_port;
     let native_cert = get_trusted_roots(&backend_vars)?;
     let mysql_pool = create_pool(&backend_vars);
@@ -113,7 +78,6 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .app_data(connector.clone())
             .service(web::scope("/api").configure(api::endpoint_config))
     })
-    // .bind(format!("0.0.0.0:{port}"), rustls_server_config)?
     .bind(format!("0.0.0.0:{port}"))?
     .run()
     .await?;
